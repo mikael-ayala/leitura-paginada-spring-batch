b36e1832709dc40dafe0b24ef5ae368d57dbfd0c
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-09-02 19:52:16 -0300

    add processor

--- user-request-sb/src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
+++ user-request-sb/src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
@@ -1,9 +1,11 @@
 package com.devsuperior.user_request_sb.step;
 
 import com.devsuperior.user_request_sb.dto.UserDTO;
+import com.devsuperior.user_request_sb.entities.User;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +25,13 @@ public class FetchUserDataAndStoreDBStepConfig {
     @Bean
     public Step fetchUserDataAndStoreDBStep(
             ItemReader<UserDTO> fetchUserDataReader,
-            ItemWriter<UserDTO> insertUserDataDBWriter,
+            ItemProcessor<UserDTO, User> selectFieldsUserDataProcessor,
+            ItemWriter<User> insertUserDataDBWriter,
             JobRepository jobRepository) {
         return new StepBuilder("fetchUserDataAndStoreDBStep", jobRepository)
-                .<UserDTO, UserDTO>chunk(chunkSize, transactionManager)
+                .<UserDTO, User>chunk(chunkSize, transactionManager)
                 .reader(fetchUserDataReader)
+                .processor(selectFieldsUserDataProcessor)
                 .writer(insertUserDataDBWriter)
                 .build();
     }
--- user-request-sb/src/main/java/com/devsuperior/user_request_sb/writer/InsertUserDataDBWriterConfig.java
+++ user-request-sb/src/main/java/com/devsuperior/user_request_sb/writer/InsertUserDataDBWriterConfig.java
@@ -1,6 +1,6 @@
 package com.devsuperior.user_request_sb.writer;
 
-import com.devsuperior.user_request_sb.dto.UserDTO;
+import com.devsuperior.user_request_sb.entities.User;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class InsertUserDataDBWriterConfig {
 
     @Bean
-    public ItemWriter<UserDTO> insertUserDataDBWriter() {
+    public ItemWriter<User> insertUserDataDBWriter() {
         return users -> users.forEach(System.out::println);
     }
 }
