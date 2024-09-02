f15b4922dac86563063bbe16368b198334ba13cf
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-09-02 20:08:54 -0300

    add multiple db configuration

--- user-request-sb/src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
+++ user-request-sb/src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
@@ -9,6 +9,7 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class FetchUserDataAndStoreDBStepConfig {
     @Autowired
+    @Qualifier("transactionManagerApp")
     private PlatformTransactionManager transactionManager;
 
     @Value("${chunkSize}")
